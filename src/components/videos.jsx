b9ac0107d7d183d6ce259482303a46a87aa9fe41
CHOIJAE HOON <hoonja@gmail.com>
2018-01-25 19:09:28 +0900

    camera on

--- src/components/videos.jsx
+++ src/components/videos.jsx
@@ -1,10 +1,33 @@
+/* global cordova */
 import React, { Component } from 'react';
 
 class Videos extends Component {
-  componentDidMount() {
-
+  turnOnCamera = () => {
+    console.log('[Videos] turnOnCamera');
+    navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true
+    })
+    .then(stream => {
+      const myVidbox = document.querySelector('#vidbox-me');
+      const vidSelf = document.createElement('VIDEO');
+      vidSelf.autoplay = true;
+      vidSelf.width = 180;
+      vidSelf.height = 240;
+      // var vidSelf = document.querySelector('#vid-self');
+      if (vidSelf) {
+        vidSelf.srcObject = stream;
+        cordova.plugins.iosrtc.refreshVideos();
+        myVidbox.appendChild(vidSelf);
+      } else {
+        console.error('Could not find the element for my video');
+      }
+    })
+    .catch(err => {
+      console.error(err);
+    });
   }
-  
+
   render() {
     return (
       <div className="row">
--- src/containers/webrtc.jsx
+++ src/containers/webrtc.jsx
@@ -1,8 +1,29 @@
+/* global cordova */
 import React, { Component } from 'react';
 import Menu from '../components/menu.jsx';
 import Videos from '../components/videos.jsx';
 
 class WebRTC extends Component {
+  componentDidMount() {
+    document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+  }
+
+  onDeviceReady = () => {
+    console.log('[WebRTC] onDeviceReady');
+    if (cordova.platformId === 'ios') {
+      cordova.plugins.iosrtc.registerGlobals();
+
+      // load adapter.js
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = 'https://webrtc.github.io/adapter/adapter-latest.js';
+      script.async = false;
+      document.getElementsByTagName("head")[0].appendChild(script);
+
+      this.videos.turnOnCamera();
+    }
+  }
+
   connect = () => {
     console.log('[WebRTC] connect');
   }
@@ -16,7 +37,7 @@ class WebRTC extends Component {
       <div>
         <p>WebRTC</p>
         <Menu onConnect={this.connect} onDisconnect={this.disconnect} />
-        <Videos />
+        <Videos ref={ref => { this.videos = ref; }} />
       </div>
     );
   }
